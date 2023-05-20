e475a2f70948ea04b0dccafebe098213d7258d62
otvertka <otvertka123@ukr.net>
2023-05-20 20:24:33 +0300

    first commit

---
+++ .history/script_20221224135721.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
---
+++ .history/script_20221228111557.js
@@ -0,0 +1,190 @@
+// 'use strict';
+
+// // Selecting elements
+// const player0El = document.querySelector('.player--0');
+// const player1El = document.querySelector('.player--1');
+// const score0El = document.querySelector('#score--0');
+// const score1El = document.getElementById('score--1');
+// const current0El = document.getElementById('current--0');
+// const current1El = document.getElementById('current--1');
+
+// // const current0Player = document.querySelector('.player--0');
+// // const current1Player = document.querySelector('.player--1');
+
+// const diceEl = document.querySelector('.dice');
+// const btnNew = document.querySelector('.btn--new');
+// const btnRoll = document.querySelector('.btn--roll');
+// const btnHold = document.querySelector('.btn--hold');
+
+// let scores, currentScore, activePlayer, playing;
+// //Staring conditions
+
+// const init = function () {
+//   scores = [0, 0];
+//   currentScore = 0;
+//   activePlayer = 0;
+//   playing = true; /*Игра продолжается */
+
+//   score0El.textContent = 0;
+//   score1El.textContent = 0;
+//   current0El.textContent = 0;
+//   current1El.textContent = 0;
+
+//   diceEl.classList.add('hidden');
+//   player0El.classList.remove('player--winner');
+//   player1El.classList.remove('player--winner');
+//   player0El.classList.add('player--active');
+//   player1El.classList.remove('player--active');
+// };
+// init();
+
+// // function of Switching Player
+// const switchPlayer = function () {
+//   document.getElementById(`current--${activePlayer}`).textContent = 0;
+//   currentScore = 0;
+//   activePlayer = activePlayer === 0 ? 1 : 0;
+//   player0El.classList.toggle('player--active');
+//   player1El.classList.toggle('player--active');
+// };
+
+// // Rolling dice functionality
+// btnRoll.addEventListener('click', function () {
+//   /*если Игра продолжается */
+//   if (playing) {
+//     // 1. Generating random dice roll
+//     const dice = Math.trunc(Math.random() * 6) + 1;
+//     console.log(dice);
+//     // 2. Display dice
+//     diceEl.classList.remove('hidden');
+//     diceEl.src = `dice-${dice}.png`;
+//     // 3. Check for rolled 1
+//     if (dice !== 1) {
+//       // Add dice to current score
+//       currentScore += dice;
+//       document.getElementById(`current--${activePlayer}`).textContent =
+//         currentScore;
+//     } else {
+//       // Switch to next player
+//       switchPlayer();
+//     }
+//   }
+// });
+
+// btnHold.addEventListener('click', function () {
+//   /*если Игра продолжается */
+//   if (playing) {
+//     // 1. Add current score to active palyer's score
+//     scores[activePlayer] += currentScore;
+
+//     // scores[1] = scores[1] + currentScore;
+//     document.getElementById(`score--${activePlayer}`).textContent =
+//       scores[activePlayer];
+//     // 2. Check if palyer's score is >=100 ?
+//     if (scores[activePlayer] >= 100) {
+//       // Finish the game
+//       playing = false;
+//       diceEl.classList.add('hidden');
+//       document
+//         .querySelector(`.player--${activePlayer}`)
+//         .classList.add('player--winner');
+//       document
+//         .querySelector(`.player--${activePlayer}`)
+//         .classList.remove('player--active');
+//     } else {
+//       // Switch to next player
+//       switchPlayer();
+//     }
+//   }
+// });
+
+// btnNew.addEventListener('click', init); /* НОВАЯ ИГРА */
+///////////////////////////////////////////////////////////////////////////
+
+// const name = 'Jonas';
+
+// const first = () => {
+//   let a = 1;
+//   const b = second(7, 9);
+//   a = a + b;
+//   return a;
+// };
+
+// function second(x, y) {
+//   var c = 2;
+//   return c;
+// }
+// const x = first();
+
+// console.log(x);
+
+// function calcAge() {
+//   const now = 2027;
+//   const age = now - birthYear;
+//   return age;
+// }
+// console.log(now);
+
+// const myName = 'Yevhen';
+
+// function first() {
+//   const age = 35;
+
+//   if (age >= 35) {
+//     const decade = 3;
+//     var millenial = true;
+//     console.log(decade);
+//   }
+
+//   function second() {
+//     const job = 'teacher';
+
+//     console.log(`${myName} is a ${age}-old ${job}`);
+//   }
+//   second();
+// }
+
+// first();
+
+// const a = 'Jonas';
+// first();
+
+// function first() {
+//   const b = 'Hello!';
+//   second();
+
+//   function second() {
+//     const c = 'Hi!';
+//     third();
+//   }
+// }
+
+// function third() {
+//   const d = 'Hey!';
+//   console.log(d + c + b + a);
+// }
+
+// const name = 'Lydiya';
+// const age = 21;
+
+// function getPersonInfo() {
+//   const name = 'Sara';
+//   const age = 22;
+//   const city = 'New York';
+
+//   return `${name} is ${age} and lives in ${city}`;
+// }
+// console.log(`${name} is ${age} and lives in ${city}`);
+
+const age = 21;
+
+function checkAge() {
+  if (age < 21) {
+    const message = 'You cannot drink!';
+    return message;
+  } else {
+    const message = 'You can drink!';
+    return message;
+  }
+}
+
+// console.log(checkAge());
